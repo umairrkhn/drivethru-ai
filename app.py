c2ea3fa84400fa07309790f3498664ebc5430543
Umair Ali Khan <umairrkhn@icloud.com>
2024-05-16 16:25:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-st.title("Swedish DriveThru Assistant")
+st.title("Svensk DriveThru Assistant")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
